97fd134679bf5a96cdeb17b64bf800f4ad38e1d4
Santhanakumar-28 <santhanakumar28@gmail.com>
2026-09-16 09:39:32 +0530

    fix(server): export db singleton reference for auto-cancel worker

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const { initializeDatabase } = require('./config/database');
+const db = require('./config/database');
+const { initializeDatabase } = db;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
